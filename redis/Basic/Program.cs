d2a04bc8bff2e27002188727107218efccc2504b
kihoonim <kihun1212@gmail.com>
2020-07-18 11:23:44 +0900

    .

---
+++ redis/Basic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StackExchange.Redis;
+using BasicPackage;
+using System.IO;
+using Google.Protobuf;
+using System.Threading;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var status = new BasicPackage.Data1 
+            { 
+                Value1 = true,
+                Value2 = "Value2",
+                Value3 = 100,
+                Value4 = 200,
+                Value5 = 10.5f
+            };
+
+            var byteString = status.ToByteString().ToStringUtf8();
+            Console.WriteLine(byteString);
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+
+            IDatabase db = redis.GetDatabase();
+
+            while (true)
+            {
+                db.ListLeftPush("key1", byteString);
+                Thread.Sleep(3 * 1000);
+            }
+        }
+    }
+}
